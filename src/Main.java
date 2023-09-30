6c98b1546e4aa23b5f6c69bd28b3a6c2e82fbb52
Johan Johnsson <johan@johnsson-net.se>
2023-09-30 22:05:27 +0200

    added players

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
     static String[] mark = {" ", " " , " ", " ", " ", " ", " ", " ", " "};
-
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        paintBoard();
+        System.out.println("LUFFARSCHACK");
+        System.out.println("1. Play" +
+                           "\n2. Quit");
+        String choice = input.next();
+        switch (choice) {
+            case "1": {
+                boolean isRunning = true;
+                while (isRunning) {
+                    paintBoard();
+                    player1();
+                    paintBoard();
+                    player2();
+                }
+                break;
+            }
+            case "2": {
+                System.exit(0);
+                break;
+            }
+        }
+
     }
 
     public static void paintBoard() {
-        System.out.println(mark[0] + " | " + mark[1] + " | " + mark[2]);
+        System.out.println(mark[0] + " | " + mark[1] + " | " + mark[2] + " // 123");
         System.out.println("_________");
-        System.out.println(mark[3] + " | " + mark[4] + " | " + mark[5]);
+        System.out.println(mark[3] + " | " + mark[4] + " | " + mark[5] + " // 456");
         System.out.println("_________");
-        System.out.println(mark[6] + " | " + mark[7] + " | " + mark[8]);
+        System.out.println(mark[6] + " | " + mark[7] + " | " + mark[8] + " // 789");
+    }
+
+    public static void player1() {
+        System.out.print("Player 1 choose a number to put your mark: ");
+        int nr = input.nextInt();
+        mark[(nr-1)] = "x";
+    }
+    public static void player2() {
+        System.out.print("Player 2 choose a number to put your mark: ");
+        int nr = input.nextInt();
+        mark[(nr-1)] = "o";
     }
 }
