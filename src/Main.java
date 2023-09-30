1673b53349c2bb5e7334c776ef471b98492cd088
Johan Johnsson <johan@johnsson-net.se>
2023-09-30 21:29:36 +0200

    created a board

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+    static String[] mark = {" ", " " , " ", " ", " ", " ", " ", " ", " "};
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        paintBoard();
+    }
+
+    public static void paintBoard() {
+        System.out.println(mark[0] + " | " + mark[1] + " | " + mark[2]);
+        System.out.println("_________");
+        System.out.println(mark[3] + " | " + mark[4] + " | " + mark[5]);
+        System.out.println("_________");
+        System.out.println(mark[6] + " | " + mark[7] + " | " + mark[8]);
     }
 }
